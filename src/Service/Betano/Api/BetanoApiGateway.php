27fa93a6464646297de3b919032d999a22ab2521
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2024-03-26 18:29:41 +0100

    Test

--- src/Service/Betano/Api/BetanoApiGateway.php
+++ src/Service/Betano/Api/BetanoApiGateway.php
@@ -21,7 +21,24 @@ class BetanoApiGateway
 
     public function getNextDailyMatchEvents(): DailyMatchEventResponse
     {
-        $header = ['User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'];
+        $header = [
+            'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0',
+            'Accept' => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            'Accept-Encoding' => "gzip, deflate, br",
+            "Accept-Language" => "de,en-US;q=0.7,en;q=0.3",
+            "Cache-Control" => "no-cache",
+            "Connection" => "keep-alive",
+            "DNT" => "1",
+            "Host" => "www.betano.de",
+            "Pragma" => "no-cache",
+            "Sec-Fetch-Dest" => "document",
+            "Sec-Fetch-Mode" => "navigate",
+            "Sec-Fetch-Site" => "none",
+            "Sec-Fetch-User" => "?1",
+            "Sec-GPC" => "1",
+            "TE" => "trailers",
+            "Upgrade-Insecure-Requests" => "1",
+        ];
         $client = $this->clientFactory->createClient($header, self::DAILY_MATCH_API_URI);
 
         $response = $client->request('GET');
