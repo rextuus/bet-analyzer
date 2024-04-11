f7b4860e77a8cae77fac0cc69ed1172cd61ed684
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2024-04-11 22:48:22 +0200

    Fix

--- src/Service/Tipico/Content/SimulationStrategy/AdditionalProcessingIdent.php
+++ src/Service/Tipico/Content/SimulationStrategy/AdditionalProcessingIdent.php
@@ -4,5 +4,6 @@ namespace App\Service\Tipico\Content\SimulationStrategy;
 
 enum AdditionalProcessingIdent: string
 {
+    case EMPTY = "";
     case STOP_NEGATIVE_SERIES = "stop_negative_series";
 }
